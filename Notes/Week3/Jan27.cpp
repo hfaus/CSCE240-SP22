0c992b5744adebe1493a3430c8bf7e86c4e14a23
hfaus <70163503+hfaus@users.noreply.github.com>
2022-01-27 10:42:23 -0500

    Add files via upload

---
+++ Notes/Week3/Jan27.cpp
@@ -0,0 +1,35 @@
+/**
+ * Name = {title, first-name, middle-name, last-name, suffix}
+ * Address = {Street name, Number, City, State, Zip code}
+ * 
+ * Note: all data members are public (anyone can edit)
+ * 
+ * Classes and Objects
+ * Class : a well defined concept
+ *      - physical or abstract
+ *      - ex : People, Students, Lectures, Dreams
+ * Object : an instance of a class
+ *      - ex : People : US-President, Dalai Lama, R2D2
+ * 
+ * Why objects/ classes?
+ * - ease of writing and understanding code
+ * - easier to test
+ * - no impact on executional performance
+ * - c++ concepts
+ *      - struct
+ *      - class (object oriented programming)
+ * - encapsulation
+ * - abstraction
+ * - separation of concerns
+ * - drive code reuse
+ * 
+ * Class : general concept that has properties that all instances share
+ *      - data members
+ *      - functions
+ * Object : specific instances
+ *      - reuse definitions of data members(variables) and methods of class
+ *      - data types and values of the member variables can be cast/ updated
+ * 
+ **/
+
+// CODE is PersonClassDemo.cpp and PersonClassDemo.h
