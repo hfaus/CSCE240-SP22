599758834dd42a086059f316c7efe7872f295b06
hfaus <70163503+hfaus@users.noreply.github.com>
2022-02-10 07:18:18 -0500

    Update documentation.cpp

--- Homework/Homework2/src/HW3/doc/documentation.cpp
+++ Homework/Homework2/src/HW3/doc/documentation.cpp
@@ -1,1 +1,8 @@
+Commands to execute:
+g++ src/OOGeometricPropertyCalculator.cpp src/Rectangle.cpp src/Shape.cpp src/Triangle.cpp src/Circle.cpp
+./a.out
 
+This code takes in an input from the user, 1 or 2. And then calculates the area(1) or perimeter(2) for a list of shapes 
+and sizes listed in input.txt. These shapes then access their respective .cpp class to calculate the area, perimeter, and
+possibly the erroMessage if an invalid input was put in. After all of the shapes have been read through, the shape name is
+read out along with either the area or perimeter to output.txt with a note at the top saying what was being displayed.
